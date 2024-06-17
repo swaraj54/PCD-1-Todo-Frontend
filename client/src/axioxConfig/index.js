32e080c1958dc63971236d910d2be2b26d49dc6e
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2024-06-17 12:32:36 +0530

    Update (deployed backend url added)

--- client/src/axioxConfig/index.js
+++ client/src/axioxConfig/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: "https://pcd-1-todo-backend.onrender.com/api/v1",
 });
 
 export default api;
